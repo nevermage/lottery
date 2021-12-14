731e048c1da96e5a92a463e40e776fd7793f02e7
bihunov <maksym.bihunov@nixsolutions.com>
2021-12-14 15:42:22 +0200

    router and view part

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Feed</title>
+    <link rel="stylesheet" href="/styles/style.css">
+</head>
+<body>
+<header>
+    <div class="headerContent">
+        <a class="headerLogo">Lottery</a>
+        <a class="headerLoginButton">Log in</a>
+    </div>
+</header>
+
+@yield('content')
+
+<footer>
+    <div class="footerContent">
+        <a class="footerLogo">Lottery</a>
+        <strong class="footerEmail">Somelottery@gmail.com</strong>
+    </div>
+</footer>
+</body>
+</html>
