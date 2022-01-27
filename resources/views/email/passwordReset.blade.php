76d51bb528d75280d5a66ee52fff9d54b86b58d6
bihunov <maksym.bihunov@nixsolutions.com>
2022-01-27 16:31:00 +0200

    removed frontend domain hardcode

--- resources/views/email/passwordReset.blade.php
+++ resources/views/email/passwordReset.blade.php
@@ -1,2 +1,2 @@
 <h2>To reset your password follow the link</h2>
-<a href="http://localhost:8080/confirm/?token=<?= $token ?>">link with token <?= $token ?></a>
+<a href="<?= env('FRONTEND_URL') ?>/password-reset/<?= $token ?>">Click here to reset your password</a>
